351da93f832112bca266afac3d347c19fc712121
Natsuki Katayama <n.katayama@mof-mof.co.jp>
2026-04-10 22:19:41 +0900

    ヘッダーとパンくずリスト追加

---
+++ web/src/components/Header.tsx
@@ -0,0 +1,11 @@
+'use client';
+
+export default function Header() {
+  return (
+		<header className="sticky top-0 px-4 bg-background">
+      <div className="flex justify-between mt-3 mb-2">
+        <h1 className="text-2xl font-bold tracking-wider text-primary">Techbook Log</h1>
+      </div>
+		</header>
+	)
+}
---
+++ web/src/components/Navigation.tsx
@@ -0,0 +1,67 @@
+"use client";
+
+import Link from "next/link";
+import { useSelectedLayoutSegments } from "next/navigation";
+import { Fragment } from "react/jsx-runtime";
+import {
+  Breadcrumb,
+  BreadcrumbItem,
+  BreadcrumbLink,
+  BreadcrumbList,
+  BreadcrumbPage,
+  BreadcrumbSeparator,
+} from "@/components/ui/breadcrumb"
+
+export const PageRoute = {
+  HOME: { pattern: /^\/books$/, label: "ホーム" },
+  BOOKS_NEW: { pattern: /^\/books\/new$/, label: "新規登録" },
+  BOOKS_DETAIL: { pattern: /^\/books\/\d+$/, label: "詳細" },
+  BOOKS_EDIT: { pattern: /^\/books\/\d+\/edit$/, label: "編集" },
+} as const
+
+function generateBreadcrumbs(segments: string[]) {
+  if (segments.length < 2) return null
+
+  let currentPath = ''
+  return segments.map((item) => {
+    currentPath += `/${item}`
+    const matched = Object.values(PageRoute).find(
+      ({ pattern }) => pattern.test(currentPath)
+    )
+    return {
+      path: currentPath,
+      label: matched?.label ?? item,
+    }
+  })
+}
+
+export default function Navigation() {
+  const segments = useSelectedLayoutSegments()
+  const breadcrumbPaths = generateBreadcrumbs(segments);
+
+  if (breadcrumbPaths === null) return
+
+  return (
+    <div className="flex item-center my-3 h-[2rem]">
+      <Breadcrumb className="h-full">
+        <BreadcrumbList className="h-full">
+          {breadcrumbPaths.map(({ path, label }, index) => (
+            <Fragment key={path}>
+              {index !== 0 && <BreadcrumbSeparator />}
+              <BreadcrumbItem>
+                {index !== breadcrumbPaths.length - 1 && (
+                  <BreadcrumbLink asChild>
+                    <Link href={path}>{label}</Link>
+                  </BreadcrumbLink>
+                )}
+                {index === breadcrumbPaths.length - 1 && (
+                  <BreadcrumbPage className="font-medium">{label}</BreadcrumbPage>
+                )}
+              </BreadcrumbItem>
+            </Fragment>
+          ))}
+        </BreadcrumbList>
+      </Breadcrumb>
+    </div>
+  )
+}
