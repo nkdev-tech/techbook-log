13312fe92a0754072b5419cfb10d95307e0f996a
Natsuki Katayama <n.katayama@mof-mof.co.jp>
2026-04-24 18:02:33 +0900

    階層変更

---
+++ web/src/app/(authenticated)/layout.tsx
@@ -0,0 +1,21 @@
+import Header from "@/components/Header";
+import Navigation from "@/components/Navigation";
+import { Toaster } from "@/components/ui/sonner";
+
+
+export default function AuthenticatedLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <>
+      <Header />
+      <main className="w-full max-w-5xl container mx-auto px-5">
+        <Navigation />
+        {children}
+      </main>
+      <Toaster />
+    </>
+  );
+}
